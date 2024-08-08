884120751344e04b145acfa15b73992c9a6c0671
IcyBlonde <haynesad4@gmail.com>
2024-08-08 19:54:14 -0400

    fully fixed input validation

--- src/main/client/Demo.java
+++ src/main/client/Demo.java
@@ -3,6 +3,7 @@ package client;
 import java.util.ArrayList;
 import java.util.Scanner;
 import game.Die;
+import java.util.InputMismatchException;
 
 public class Demo {
     public static void main(String[] args){
@@ -20,22 +21,27 @@ public class Demo {
         Scanner scanny = new Scanner(System.in);
         ArrayList<Die> reroll = new ArrayList<>();
         ArrayList<Integer> used = new ArrayList<>();
-        System.out.println("Please enter the number of the die you would like to reroll");
-        System.out.println("Enter 0 to quit.");
-        int input = scanny.nextInt();
-        while(input < 0 && input > 5 && ! used.contains(input)){
-            System.out.println("Please enter a valid  number.");
-            input = scanny.nextInt();
-        }
-        while(input != 0){
-            used.add(input);
-            reroll.add(die.get(input - 1));
-            die.remove(input - 1);
 
-            System.out.println("Please enter the number of the die you would like to reroll");
-            System.out.println("Enter 0 to quit.");
-            input = scanny.nextInt();
-        }
+        int input = -1;
+        do {
+            try {
+                System.out.println("Please enter the number of the die you would like to reroll (1-5).");
+                System.out.println("Enter 0 to quit.\n");
+                input = scanny.nextInt();
+    
+                if (input >= 1 && input <= 5 && !used.contains(input)) {
+                    used.add(input);
+                    reroll.add(die.get(input - 1));
+                    die.remove(input - 1);
+                } else if (input != 0) {
+                    System.out.println("Invalid input or die already selected. Please try again.\n");
+                }
+            } catch (InputMismatchException e) {
+                System.out.println("Invalid input. Please enter a valid number.\n");
+                scanny.next(); // Clear the invalid input from the scanner
+                input = -1; // Reset input to force retry
+            }
+        } while (input != 0);
         Die.rollDie(reroll);
 
 
