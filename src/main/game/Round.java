5181489a3c25d0f367178ed0717de383492db9e3
IcyBlonde <haynesad4@gmail.com>
2024-08-09 12:07:16 -0400

    fixed data structure holding scoresheets

--- src/main/game/Round.java
+++ src/main/game/Round.java
@@ -2,10 +2,12 @@ package game;
 
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Round {
     public static ArrayList<String> players = new ArrayList<>();
-    public static ArrayList<Scoresheet> sheets = new ArrayList<>();
+    public static Map<String, Scoresheet> sheets = new HashMap<>();
 
     public static void setNames(ArrayList<String> players){
         Scanner scanny = new Scanner(System.in);
@@ -17,9 +19,9 @@ public class Round {
         } while (input != "n");
     }
 
-    public static void makeSheets(ArrayList<String> players, ArrayList<Scoresheet> sheets){
-        for (String player : players) {
-            sheets.add(new Scoresheet(player));
+    public static void makeSheets(ArrayList<String> players, Map<String, Scoresheet> sheets){
+        for(int i = 0; i < players.size(); i++){
+            sheets.put("sheet" + (i +1), new Scoresheet(players.get(i)));
         }
     }
 }
