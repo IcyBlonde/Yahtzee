da6790b05398fc3ff31f6919015ea33eabf67024
IcyBlonde <haynesad4@gmail.com>
2024-08-13 12:29:56 -0400

    started GUI

--- src/main/client/Demo.java
+++ src/main/client/Demo.java
@@ -1,15 +1,31 @@
 package client;
 
 import java.util.ArrayList;
-import java.util.Scanner;
+import javax.swing.*;
 import game.*;
 
 public class Demo {
+
+    public static JFrame Yahtzee; 
+    public static JLabel intro;
+    public static JTextField name;
+    public static JPanel playerSelect;
+    public static JButton next;
+    public static JButton done;
     public static void main(String[] args){
-        ArrayList<Die> current = new ArrayList<>();
-        Die.fill(current);
-        Die.rollDie(current);
-        Die.redo(current);
-        Die.readDie(current);
+        Yahtzee = new JFrame();
+        Yahtzee.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        Yahtzee.setSize(500, 500);
+        playerSelect = new JPanel();
+        Yahtzee.add(playerSelect);
+        intro = new JLabel("Input Player Name Then Done");
+        playerSelect.add(intro);
+        name = new JTextField();
+        playerSelect.add(name);
+        next = new JButton("Next");
+        done = new JButton("Done");
+        playerSelect.add(next);
+        playerSelect.add(done);
+        Yahtzee.setVisible(true);
     }
 }
